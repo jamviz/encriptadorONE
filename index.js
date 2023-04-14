ca26155c6cb8ea93c11afcf85c9b62fa24289873
Jamir Balcona Viza <74891753+jamviz@users.noreply.github.com>
2023-04-14 00:41:57 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+    let cifrados = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+
+    if (texto.length != 0) {
+      document.getElementById("texto").value = cifrados;
+      tituloMensaje.textContent = "Texto encriptado listo!";
+      parrafo.textContent = "";
+      muñeco.src = "./img/encriptado.jpg";
+      
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Mensaje vacío";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+  }
+  
+  function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let cifrados = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = cifrados;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.jpg";
+
+      } else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Mensaje Vacío";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+  }
